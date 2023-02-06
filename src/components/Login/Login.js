1d4221a601f5f1982d092126923dab3796e7ab17
rampion19 <princegupta.pg19@gmail.com>
2023-02-06 14:38:03 +0530

    Login page project with useEffect hook

---
+++ src/components/Login/Login.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from 'react';
+
+import Card from '../UI/Card/Card';
+import classes from './Login.module.css';
+import Button from '../UI/Button/Button';
+
+const Login = (props) => {
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [emailIsValid, setEmailIsValid] = useState();
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [passwordIsValid, setPasswordIsValid] = useState();
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [enteredCollageName, setEnteredCollageName] = useState('');
+  const [collageNameIsValid, setCollagenameIsValid] = useState();
+
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes('@') &&
+      enteredPassword.trim().length > 6 &&
+      enteredCollageName.trim().length > 1
+    )
+  }, [enteredEmail, enteredPassword, enteredCollageName])
+
+  const emailChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const passwordChangeHandler = (event) => {
+    setEnteredPassword(event.target.value);
+  };
+  const collageNameChangeHandler = (event) => {
+    setEnteredCollageName(event.target.value);
+  };
+
+  const validateEmailHandler = () => {
+    setEmailIsValid(enteredEmail.includes('@'));
+  };
+
+  const validatePasswordHandler = () => {
+    setPasswordIsValid(enteredPassword.trim().length > 6);
+  };
+
+  const validateCollageNameHandler = () => {
+    setCollagenameIsValid(enteredCollageName.trim().length > 1);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    props.onLogin(enteredEmail, enteredPassword);
+  };
+
+  return (
+    <Card className={classes.login}>
+      <form onSubmit={submitHandler}>
+        <div
+          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={validateEmailHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${passwordIsValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={enteredPassword}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
+        </div>
+        <div
+          className={`${classes.control} ${collageNameIsValid === false ? classes.invalid : ''
+            }`}
+        >
+          <label htmlFor="collage">Collage Name</label>
+          <input
+            type="text"
+            id="collage"
+            value={enteredCollageName}
+            onChange={collageNameChangeHandler}
+            onBlur={validateCollageNameHandler}
+          />
+        </div>
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+            Login
+          </Button>
+        </div>
+      </form>
+    </Card>
+  );
+};
+
+export default Login;
